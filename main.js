0a380d853e8c373cfb2b7f41680149b613b228c5
midmat4828@gmail.com <midmat4828@gmail.com>
2023-11-13 22:45:45 +0530

    test

---
+++ main.js
@@ -0,0 +1,21 @@
+const userChoiceDisplay = document.getElementById('user-choice');
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const result = document.getElementById('result');
+let randomNumber;
+
+randomNumber = Math.random() * 3 + 1;
+randomNumber = Math.floor(randomNumber);
+
+if (randomNumber == 1)
+{
+	userChoiceDisplay.innerHTML = 'rock';
+}
+if (randomNumber == 2)
+{
+	userChoiceDisplay.innerHTML = 'paper';
+}
+if (randomNumber == 3)
+{
+	userChoiceDisplay.innerHTML = 'scissor';
+}
+
